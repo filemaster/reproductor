199d77c5c611e9362fd86731b1045e10af4fa591
Ivan Duran <ivan.duran@indenext.com>
2016-04-13 19:10:56 +0200

    Initial Commit

---
+++ Reproductor/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Reproductor
+//
+//  Created by Ivan Duran Martinez on 13/4/16.
+//  Copyright © 2016 Ivan Duran Martinez. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
